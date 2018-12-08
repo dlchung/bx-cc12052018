d9c24ca291733a877ab7978a8d925c06f372f8f3
Dan Chung <danetix@gmail.com>
2018-12-08 14:29:07 -0500

    restructure code

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function getCartData() {
   var cartTotal = document.querySelector(".order-value").innerText;
   var itemImages = document.getElementsByClassName("mini-cart-image");
   var imageUrls = new Array;
-
+  
+  for(i = 0; i < itemImages.length; i++) {
+    imageUrls.push(itemImages.item(i).querySelector("img").src);
+  }
+  
   var dataObj = {
     numItems: numItems,
     cartTotal: cartTotal,
@@ -11,10 +15,6 @@ function getCartData() {
     imageUrls: imageUrls
   }
 
-  for(i = 0; i < itemImages.length; i++) {
-    imageUrls.push(itemImages.item(i).querySelector("img").src);
-  }
-  
   // console.log("Number of items: ", numItems);
   // console.log("Cart total: ", cartTotal);
   // console.log("Item images: ", itemImages);
@@ -38,89 +38,102 @@ function setTrigger() {
     var scrollPos = document.body.clientHeight - event.path[1].innerHeight; // total scrollable height
     var scrollTrigger = scrollPos - (scrollPos * 0.1); // triggered scroll pos
 
+    // this.console.log(scrollY, scrollTrigger);
+
     if(scrollTrigger < scrollY && overlayTriggered === false) {
-      showOverlay(document.body.clientHeight);
+    // if(scrollTrigger < scrollY) {
+      // renderOverlay(document.body.clientHeight);
+      showOverlay();
     }
   })
+}
 
-  function showOverlay(clientHeight) {
-    overlayTriggered = true;
-    var dataObj = getCartData();
+function renderOverlay(clientHeight) {
+  var dataObj = getCartData();
 
-    var overlayWrap = document.createElement("div");
-    var overlayStyle = overlayWrap.style;
-    
-    overlayWrap.className = "overlay-wrap";
-    overlayStyle.background = "#000";
-    overlayStyle.height = clientHeight + "px";
-    overlayStyle.left = "0";
-    overlayStyle.opacity = "0.6";
-    overlayStyle.position = "absolute";
-    overlayStyle.top = "0";
-    overlayStyle.width = "100vw";
-    overlayStyle.zIndex = "100";
+  var overlayWrap = document.createElement("div");
+  var overlayStyle = overlayWrap.style;
+  
+  overlayWrap.className = "overlay-wrap";
+  overlayStyle.background = "#000";
+  overlayStyle.display = "none";
+  overlayStyle.height = clientHeight + "px";
+  overlayStyle.left = "0";
+  overlayStyle.opacity = "0.6";
+  overlayStyle.position = "absolute";
+  overlayStyle.top = "0";
+  overlayStyle.width = "100vw";
+  overlayStyle.zIndex = "100";
 
-    document.body.appendChild(overlayWrap);
+  document.body.appendChild(overlayWrap);
 
-    var modal = document.createElement("div");
-    var modalStyle = modal.style;
-    var modalHeight = "500";
-    var modalWidth = "500";
-    var unit = "px";
-    
-    modal.className = "overlay-modal";
-    modalStyle.background = "#fff";
-    modalStyle.border = "solid 5px #ddd";
-    modalStyle.borderRadius = "10px";
-    modalStyle.boxSizing = "border-box";
-    modalStyle.left = "50%";
-    modalStyle.height = modalHeight + unit;
-    modalStyle.width = modalWidth + unit;
-    modalStyle.padding = "10px";
-    modalStyle.position = "fixed";
-    modalStyle.top = "50%";
-    modalStyle.zIndex = "101";
-    modalStyle.marginTop = "-" + (modalHeight / 2) + "px";
-    modalStyle.marginLeft = "-" + (modalWidth / 2) + "px";
+  var modal = document.createElement("div");
+  var modalStyle = modal.style;
+  var modalHeight = "500";
+  var modalWidth = "500";
+  var unit = "px";
+  
+  modal.className = "overlay-modal";
+  modalStyle.background = "#fff";
+  modalStyle.border = "solid 5px #ddd";
+  modalStyle.borderRadius = "10px";
+  modalStyle.boxSizing = "border-box";
+  modalStyle.display = "none";
+  modalStyle.left = "50%";
+  modalStyle.height = modalHeight + unit;
+  modalStyle.width = modalWidth + unit;
+  modalStyle.padding = "10px";
+  modalStyle.position = "fixed";
+  modalStyle.top = "50%";
+  modalStyle.zIndex = "101";
+  modalStyle.marginTop = "-" + (modalHeight / 2) + "px";
+  modalStyle.marginLeft = "-" + (modalWidth / 2) + "px";
 
-    var renderImages = dataObj.imageUrls.map(function(url) {
-      return "<img src='" + url + "'" +
-        "style='border: solid 2px #eee; margin: 5px;'" +
-      " />";
-    })
+  var renderImages = dataObj.imageUrls.map(function(url) {
+    return "<img src='" + url + "'" +
+      "style='border: solid 2px #eee; margin: 5px;'" +
+    " />";
+  })
 
-    var pStyle = "font-size: 1.3em; padding-bottom: 10px;";
-    var renderCartData = "<div class='overlay-cart-data' style=''>" +
-      "<p class='overlay-data' style='" + pStyle + "'>You've reached the end! Would you like to checkout?</p>" +
-      "<p class='overlay-data' style='" + pStyle + "'>You have " + dataObj.numItems + " products in your cart.</p>" +
-      "<p class='overlay-data' style='" + pStyle + "'>Total: " + dataObj.cartTotal + "</p>" +
-    "</div>";
+  var pStyle = "font-size: 1.3em; padding-bottom: 10px;";
+  var renderCartData = "<div class='overlay-cart-data' style=''>" +
+    "<p class='overlay-data' style='" + pStyle + "'>You've reached the end! Would you like to checkout?</p>" +
+    "<p class='overlay-data' style='" + pStyle + "'>You have " + dataObj.numItems + " products in your cart.</p>" +
+    "<p class='overlay-data' style='" + pStyle + "'>Total: " + dataObj.cartTotal + "</p>" +
+  "</div>";
 
-    var renderClose = "<div class='overlay-close-modal' style='color: #666; font-size: 18px; font-weight: bold; padding-bottom: 10px; text-align: right;'>" + 
-      "<a style='background: #eee; border-radius: 10px; display: inline-block; padding: 5px 8px; text-align: center;'" +
-      "class='overlay-close-btn'" +
-      "onclick='" + destroyOverlay + "'>X</a>" +
-    "</div>";
+  var renderClose = "<div class='overlay-close-modal' style='color: #666; font-size: 18px; font-weight: bold; padding-bottom: 10px; text-align: right;'>" + 
+    "<a style='background: #eee; border-radius: 10px; display: inline-block; padding: 5px 8px; text-align: center;'" +
+    "class='overlay-close-btn'>X</a>" +
+  "</div>";
 
-    var renderCartBtn = "<div><button>Go to Cart</button></div>";
+  var renderCartBtn = "<div>" +
+    "<button class='overlay-cart-btn' style='background: red; color: #fff; padding: 5px 10px; text-transform: uppercase;'>Go to Cart</button>" +
+  "</div>";
 
-    modal.innerHTML = (
-      renderClose +
-      renderCartData +
-      renderImages +
-      renderCartBtn
-    );
+  modal.innerHTML = (
+    renderClose +
+    renderCartData +
+    renderImages +
+    renderCartBtn
+  );
 
-    console.log(document.querySelector(".overlay-close-btn"));
-    document.body.appendChild(modal);
-  }
+  document.body.appendChild(modal);
+  document.querySelector(".overlay-close-btn").onclick = hideOverlay;
+}
 
-  function destroyOverlay() {
-    console.log("hi");
-  }
+function hideOverlay() {
+  overlayTriggered = false;
+  document.querySelector(".overlay-wrap").style.display = "none";
+  document.querySelector(".overlay-modal").style.display = "none";
+}
 
+function showOverlay() {
+  overlayTriggered = true;
+  document.querySelector(".overlay-wrap").style.display = "inline";
+  document.querySelector(".overlay-modal").style.display = "inline";
 }
 
 var overlayTriggered = false;
 setTrigger();
-// document.querySelector(".overlay-wrap").style.display = "none";
+renderOverlay(document.body.clientHeight);
